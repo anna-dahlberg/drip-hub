51c62ee857e1b123605f0475a96ae61af05c18af
anna-dahlberg <anna.dahlberg@live.se>
2025-02-26 11:12:17 +0100

    updated Product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,9 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        return view('products.index');
+
+        return view('products.index', [
+            'products' => $products
+        ]);
     }
 }
