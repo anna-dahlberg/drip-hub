debc66b4189829c9a5d176a9a87e36d0170a8c7d
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-03-05 12:20:39 +0100

    added basic html and php to product page

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,13 +1,25 @@
 <x-layout>
+    <h1>Product information</h1>
+    <img src="https://vanbruun.com/__media-service/img/product/1218/jacob-30x17-yg_hover.jpg" alt="Image of ring model xxxxxx">
+    <h2>{{ $product->ModelRing->name }}</h2>
 
-    <h1>{{ $product->name }}</h1>
-
+    <p class="product-title">Article number</p>
     <p>{{ $product->article_number }}</p>
 
-    <p>{{ $product->size }}</p>
+    <p class="product-title">Ring type</p>
+    <p>{{ $product->ringType->type }}</p>
 
-    <p>{{ $product->ringType }}</p>
+    <p class="product-title">Price</p>
+    <p>{{ $product->price }}:-</p>
 
-    <p>{{ $product->price }}</p>
-    
+    <p class="product-title">Size</p>
+    <select name="size" id="product-size">
+        <?php for ($i = 14; $i <= 23; $i++): ?>
+            <option value="<?= $i; ?>"><?= $i; ?></option>
+        <?php endfor; ?>
+    </select>
+
+    <p class="product-title">In stock</p>
+    <p>{{ $product->stock }}</p>
+
 </x-layout>
