5edd2ca5679376b170b0dca0bd7a1e5f9c2f24b2
anna-dahlberg <anna.dahlberg@live.se>
2025-03-10 13:56:36 +0100

    completed test_create_product and test passed

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -2,11 +2,31 @@
 
 namespace Tests\Feature;
 
+use App\Models\Material;
+use App\Models\ModelRing;
+use App\Models\RingType;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductControllerTest extends TestCase
 {
-    public function create_product_test() {}
+    use RefreshDatabase;
+
+    public function test_create_product()
+    {
+        $types = RingType::factory()->count(3)->create();
+        $materials = Material::factory()->count(3)->create();
+        $models = ModelRing::factory()->count(3)->create();
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->get(route('products.create'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('products.create');
+        $response->assertViewHasAll(['types', 'materials', 'models']);
+    }
 }
