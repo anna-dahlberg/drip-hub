0d0afb0e4e8105e7eef4d5be7c2fab63e83f3d8e
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-03-11 23:42:06 +0100

    changed to eng on title and aded image on home-page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <x-layout>
 
-<h1>Drip Hub</h1>
-<h2>Välkommen!</h2>
-
+    <h1>Drip Hub</h1>
+    <h2>Welcome!</h2>
+    <img class="welcome-img" src="https://vanbruun.com/__media-service/img/cms/861/book-appointment-consultation-image-38.jpg" alt="">
 </x-layout>
-
