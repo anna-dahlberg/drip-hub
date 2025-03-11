80ce019b3e8f8bc665e300df184cec153dac4330
TobiasAndren <tobiasandren@hotmail.com>
2025-03-11 16:23:56 +0100

    added error layout to user forms

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
-<form method="POST" class="user-form" action="{{ route('users.store')}}">
-<x-users.form :user="$user ?? null" :roles="$roles" :addPassword="true"></x-users.form>
-</form>
+
+    <x-errors />
+
+
+    <form method="POST" class="user-form" action="{{ route('users.store')}}">
+        <x-users.form :user="$user ?? null" :roles="$roles" :addPassword="true"></x-users.form>
+    </form>
+
 </x-layout>
