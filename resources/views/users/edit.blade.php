68ba4e36d2a760bcd018903d6be6dcb711c7744e
TobiasAndren <tobiasandren@hotmail.com>
2025-03-11 15:01:10 +0100

    fixed user.edit bug

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,11 +2,11 @@
 <form method="POST" class="user-form" action="{{ route('users.update', $user)}}">
     @method('PATCH')
     <x-users.form :user="$user" :roles="$roles" :addPassword="false"></x-users.form>
-    <form method="POST" action="{{ route('users.destroy', $user) }}" 
+</form>
+<form method="POST" action="{{ route('users.destroy', $user) }}" 
     onsubmit="return confirm('Are you sure you want to delete this user?')">
-        @method('DELETE')
-        @csrf
-        <button type="submit" class="user-delete-button">Delete</button>
-    </form>
+    @method('DELETE')
+    @csrf
+    <button type="submit" class="user-delete-button">Delete</button>
 </form>
 </x-layout>
